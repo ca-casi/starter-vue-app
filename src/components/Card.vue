1a95afd54f80f6134ff1eef5ce27de36f962e110
Carlos Oliveira <cardevisi@gmail.com>
2019-02-04 11:49:40 -0200

    adding files to the project

---
+++ src/components/Card.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="card">
+    <h3>{{ title }}</h3>
+    <ul>
+      <li v-for="photo in photos">
+        <label @click="onClick"></label>
+        <img width="50px" :src="photo.url" :alt="photo.title" @click="onClick"/>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Card',
+    props: {
+      title: String
+    },
+    data() {
+      return {
+        photos: [
+          {
+            url: 'http://s2.glbimg.com/lCgKRgb9XrRUp48TMCaHiVRy0k4=/e.glbimg.com/og/ed/f/original/2017/05/30/thinkstockphotos-646108884.jpg',
+            title: 'Lorem upseum'
+          },
+          {
+            url: 'http://s2.glbimg.com/lCgKRgb9XrRUp48TMCaHiVRy0k4=/e.glbimg.com/og/ed/f/original/2017/05/30/thinkstockphotos-646108884.jpg',
+            title: 'Lorem upseum'
+          }
+        ]
+      }
+    },
+    methods: {
+      onClick: function() {
+        alert('Clicou');
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h3 {
+    margin: 40px 0 0;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
+</style>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Lembrei que um professor no curso de desenvolvimento, sempre construia uma aplicação de gerenciamento de musicas para testar uma novo tecnologia.
---
+++ src/constantes/colors.js
@@ -0,0 +1,11 @@
+export const ALL_COLORS = [
+  'text', 'white', 'grey-dark', 'grey', 'grey-light', 'blue-dark',
+  'blue', 'blue-semi-light', 'blue-light', 'green-dark', 'green',
+  'green-semi-light', 'green-light', 'red-dark', 'red', 'red-semi-light',
+  'red-light', 'orange-dark', 'orange-semi-dark', 'orange', 'orange-semi-light',
+  'orange-light', 'yellow-dark', 'yellow', 'yellow-semi-light', 'yellow-light',
+];
+
+export const PRIMARY_COLORS = [
+  'blue', 'green', 'red', 'yellow', 'grey', 'text',
+];
